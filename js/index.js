235fc3dbed3b40c74f60f75d8b1b3f7f6c70ae5a
liangtianfu <liangtianfu985@126.com>
2019-06-22 18:28:33 +0800

    轮播图完成

--- js/index.js
+++ js/index.js
@@ -2,13 +2,26 @@ window.onload = function () {
   init();
 
   function init() {
+    initSwiper();
     eventList();
   }
-  
+
   // 点击显示隐藏
   function eventList() {
     document.querySelector(".header_menu").onclick = function () {
       document.querySelector(".main_menu").classList.toggle("hidden");
     }
+  }
+  // 轮播图
+  function initSwiper() {
+    var mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination'
+      },
+      autoplay: {
+        disableOnInteraction: false
+      }
+    })
   }
 }
